891d327ee4ceef78a804c9eeba6e0256bb1de54c
Luis Guillermo Custodio <121039302@upq.edu.mx>
2024-05-10 17:11:15 -0600

    If

---
+++ Intro/03-control-flujo/01-if.js
@@ -0,0 +1,20 @@
+/*
+if()condicion
+    expresion
+
+if(condicion){
+    expresiones
+}
+*/
+
+let edad = 25
+
+if (edad > 17) {
+    console.log('usuario mayor de edad')
+}
+
+edad = 10
+
+if (edad > 17) {
+    console.log('usuario mayor de edad')
+}
