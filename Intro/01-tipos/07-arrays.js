4c753752cd4fafd3d758369fabe92babbd8e5c63
Luis Guillermo Custodio <121039302@upq.edu.mx>
2024-05-10 16:11:01 -0600

    Arreglos

---
+++ Intro/01-tipos/07-arrays.js
@@ -0,0 +1,13 @@
+let animales = ['perro', 'gato'];
+
+console.log(animales);
+console.log(animales[0]);
+
+animales[2] = 'tortuga';
+console.log(animales);
+
+animales[10] = 'tortuga';
+console.log(animales[7]);
+
+console.log(typeof animales);
+console.log(animales.length);
