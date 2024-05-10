0af02eb9c426051b67267df2d2c844abba86a5f3
Luis Guillermo Custodio <121039302@upq.edu.mx>
2024-05-10 15:43:03 -0600

    Elaboracion de variables

---
+++ Intro/01-tipos/01-variables.js
@@ -0,0 +1,29 @@
+// let -> palabra reservada para variables
+//js es case sensitive
+let nombre = 'Hola Mundo';
+let NombreCompleto;
+let nombreCompleto;
+let nombre_completo;
+
+console.log(nombre);
+
+let apellido;
+let animal;
+apellido = 'Custodio';
+
+/*
+Tipos de datos:
+
+Primitivos:
+    Number
+    String
+    Boolean
+    Undefined
+    Null
+
+De referencia;
+    Array
+    Object
+    Functions
+    Class
+ */
