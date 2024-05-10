c43577e2e9645f3ac8bca7fea69f90c6be1043eb
Luis Guillermo Custodio <121039302@upq.edu.mx>
2024-05-10 17:11:46 -0600

    Else y else if

---
+++ Intro/03-control-flujo/02-else.js
@@ -0,0 +1,57 @@
+/*
+if()condicion
+    expresion
+else if (condicion)
+    expresion
+else
+    expresion
+
+
+
+
+if(condicion){
+    expresiones
+}
+else if(condicion){
+    expresiones
+}
+else{
+    expresiones
+}
+*/
+
+let edad = 25;
+
+if (edad > 17) {
+    console.log('usuario mayor de edad')
+}
+else if (edad > 13) {
+    console.log('usuario debe de estar acompañado de un mayor de edad')
+}
+else {
+    console.log('usuario menor de edad')
+}
+
+edad = 15;
+
+if (edad > 17) {
+    console.log('usuario mayor de edad')
+}
+else if (edad > 13) {
+    console.log('usuario debe de estar acompañado de un mayor de edad')
+}
+else {
+    console.log('usuario menor de edad')
+}
+
+edad = 10;
+
+if (edad > 17) {
+    console.log('usuario mayor de edad')
+}
+else if (edad > 13) {
+    console.log('usuario debe de estar acompañado de un mayor de edad')
+}
+else {
+    console.log('usuario menor de edad')
+}
