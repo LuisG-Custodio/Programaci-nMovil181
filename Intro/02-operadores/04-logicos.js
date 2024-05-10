4434d9aaa3c24163aa4a803fd034f545645c09a9
Luis Guillermo Custodio <121039302@upq.edu.mx>
2024-05-10 16:46:46 -0600

    Operadores lógicos

---
+++ Intro/02-operadores/04-logicos.js
@@ -0,0 +1,20 @@
+//and
+
+/* console.log(true && false)
+console.log(false && false)
+console.log(true && true) */
+
+let mayor = true;
+let suscrito = false;
+console.log(mayor && suscrito);
+
+//or
+
+/* console.log(true || false)
+console.log(false || false)
+console.log(true || true) */
+
+console.log(mayor || suscrito);
+
+//not
+console.log(!mayor);
