8c97621da0f8901e7a64d39e3a33a4cb6732caef
Luis Guillermo Custodio <121039302@upq.edu.mx>
2024-05-10 23:17:10 -0600

    Introduccion a OOP

---
+++ Intro/05-objetos/01-intro.js
@@ -0,0 +1,19 @@
+/* let email = 'example@email.com';
+let name = 'Luis';
+let direccion = {
+    calle: 'Anillo vial',
+    numero: 4
+}; */
+
+let user = {
+    email: 'example@email.com',
+    name: 'Luis',
+    direccion: {
+        calle: 'Anillo vial',
+        numero: 4
+    },
+    activo: true,
+    recuperarClave: function () {
+        console.log("Recuperando clave...")
+    }
+};
