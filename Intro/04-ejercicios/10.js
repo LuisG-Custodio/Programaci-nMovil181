deb7bfda11b339920f3f5080827e0baa23f76cbd
Luis Guillermo Custodio <121039302@upq.edu.mx>
2024-05-10 22:59:43 -0600

    Ejercicio 10

---
+++ Intro/04-ejercicios/10.js
@@ -0,0 +1,14 @@
+let longitud = 9;
+
+function crearArray(n) {
+    if (n <= 0) {
+        return [];
+    }
+    array = []
+    for (let i = 0; i < n; i++) {
+        array[i] = i + 1;
+    }
+    return array;
+}
+let resultado = crearArray(longitud);
+console.log(resultado);
